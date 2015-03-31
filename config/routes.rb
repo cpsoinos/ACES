d76a2fe1dce570f8be9a36074bf8006e3e5f0b57
elliott-ribner <elr886@gmail.com>
2015-03-31 15:52:25 -0400

    fixed hound and travis

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :restaurants, only: [
-    :index, :show:, :new, :edit,
+    :index, :show, :new, :edit,
     :create, :update, :destroy
     ]
   root "restaurants#index"
