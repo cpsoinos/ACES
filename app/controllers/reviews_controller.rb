50cd92c8a1e8238d829f096b7ab2df5c876bec17
Corey Psoinos <coreypsoinos@gmail.com>
2015-04-01 16:07:22 -0400

    Final updates to review and restaurant testing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -55,7 +55,7 @@ class ReviewsController < ApplicationController
 
   private
 
-    def review_params
-      params.require(:review).permit(:rating, :body)
-    end
+  def review_params
+    params.require(:review).permit(:rating, :body)
+  end
 end
