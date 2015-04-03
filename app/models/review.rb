71e279f238b07f61408dc5183093f4f80b332889
Corey Psoinos <coreypsoinos@gmail.com>
2015-04-03 16:08:13 -0400

    Add dependend: :destroy to reviews (to destroy its votes)

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
   belongs_to :user
-  has_many :votes
+  has_many :votes, dependent: :destroy
 
   validates :body, presence: true
   validates :rating, presence: true
