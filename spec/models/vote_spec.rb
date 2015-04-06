1edec3d157ba4073eefd10aada92cd3fb700044b
Corey Psoinos <coreypsoinos@gmail.com>
2015-04-05 22:38:40 -0400

    Appease the hound

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
 
 RSpec.describe Vote, type: :model do
-  it 'increments score' do
+  it "increments score" do
     vote = FactoryGirl.create(:vote)
     vote.increment
     expect(vote.score).to eq 1
   end
-  it 'decrements score' do
+  it "decrements score" do
     vote = FactoryGirl.create(:vote)
     vote.decrement
     expect(vote.score).to eq (-1)
