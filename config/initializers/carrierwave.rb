db3a7f061acb746858c0e2453c7c84644861a4d9
Corey Psoinos <coreypsoinos@gmail.com>
2015-04-08 10:01:48 -0400

    Hound compliance

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,16 +5,13 @@ CarrierWave.configure do |config|
     config.storage :fog
 
     config.fog_credentials = {
-      provider:              "AWS",                        # required
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],                        # required
-      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],                        # required
-      # region:                "US Standard",                  # optional, defaults to "us-east-1"
-      # host:                  "s3.example.com",             # optional, defaults to nil
-      # endpoint:              "https://s3.example.com:8080" # optional, defaults to nil
-    }
-    config.fog_directory  = ENV["S3_BUCKET"]                             # required
-    config.fog_public     = false                                        # optional, defaults to true
-    config.fog_attributes = { "Cache-Control" => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+      provider:              "AWS",
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+        }
+    config.fog_directory  = ENV["S3_BUCKET"]
+    config.fog_public     = false
+    config.fog_attributes = { "Cache-Control" => "max-age=#{365.day.to_i}" }
   else
     config.storage :file
   end
