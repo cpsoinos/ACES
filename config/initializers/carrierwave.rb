1c5ff5bd40d4f77cf5f11058f654e22a4101935a
Corey Psoinos <coreypsoinos@gmail.com>
2015-04-08 10:06:17 -0400

    Hound compliance

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,7 @@ CarrierWave.configure do |config|
       provider:              "AWS",
       aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-        }
+    }
     config.fog_directory  = ENV["S3_BUCKET"]
     config.fog_public     = false
     config.fog_attributes = { "Cache-Control" => "max-age=#{365.day.to_i}" }
--- spec/models/avatar_spec.rb
+++ spec/models/avatar_spec.rb
@@ -17,13 +17,13 @@ describe AvatarUploader do
     @uploader.remove!
   end
 
-  context 'the thumb version' do
+  context "the thumb version" do
     it "should scale down a landscape image to be exactly 64 by 64 pixels" do
       @uploader.thumb.should have_dimensions(64, 64)
     end
   end
 
-  context 'the small version' do
+  context "the small version" do
     it "should scale down a landscape image to fit within 200 by 200 pixels" do
       @uploader.small.should be_no_larger_than(200, 200)
     end
